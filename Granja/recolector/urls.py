44d86981d85c6669cef44e8730777c9585842785
daferarte <daferarte@gmail.com>
2023-11-11 18:29:16 -0500

    agregando editar y eliminar

--- Granja/recolector/urls.py
+++ Granja/recolector/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('agregar',views.agregar, name='agregar_recolector'),
     path('mostrar',views.mostrar, name='mostrar_recolector'),
+    path('ver/<int:pk>',views.editar, name='ver_recolector'),
+    path('eliminar/<int:pk>',views.eliminar, name='eliminar_recolector'),
 ]
--- Granja/recolector/views.py
+++ Granja/recolector/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Recolector
 
@@ -37,3 +37,39 @@ def mostrar(request):
         'title': 'Recolectores',
         'recolectores': recolectores
     })
+    
+def editar(request, pk):
+    
+    if request.method == 'POST':
+        
+        cedula=request.POST.get('cedula')
+        nombres=request.POST.get('nombres')
+        apellidos=request.POST.get('apellidos')
+        direccion=request.POST.get('direccion')
+        fecnac=request.POST.get('fecnac')
+        telefono=request.POST.get('telefono')
+        
+        miRecolector=Recolector.objects.filter(pk=pk).update(
+            cedula=cedula, 
+            nombres=nombres,
+            apellido=apellidos,
+            direccion=direccion,
+            fNacimiento=fecnac,
+            telefono=telefono
+        )
+        
+        messages.success(request, 'Actualizado correctamente')
+        return redirect('mostrar_recolector')
+    
+    recolector = Recolector.objects.get(pk=pk)
+    
+    return render(request, 'recolector/editar.html',{
+        'title': 'Recolectores',
+        'recolector': recolector
+    })
+    
+def eliminar(request, pk):
+    recolector= get_object_or_404(Recolector, pk=pk)
+    recolector.delete()
+    messages.success(request, 'Borrado correctamente')
+    return redirect('mostrar_recolector')
